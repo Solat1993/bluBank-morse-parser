a711af5ff1d667c3bdb8b5664bb05ec548865b9d
solat1993 <s.houshmand93@gmail.com>
2021-11-13 01:32:14 +0330

    add a class as message broker

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from constants import Const
-from flask import Flask
+from flask import Flask, jsonify
+import pika
 
-# creating app
 app = Flask(__name__)
 constant = Const()
 
 
-class Morse:
+class MorseService:
 
     @staticmethod
     @app.route('/encrypt/<string:message>/', methods=['GET', 'POST'])
@@ -15,18 +15,45 @@ class Morse:
         for letter in message:
             if letter != ' ':
                 cipher += constant.MORSE_CODE_DICT[letter.upper()] + ' '
-
             else:
                 cipher += ' '
 
-        return cipher
+        return jsonify(
+            {
+                "message": message,
+                "cipher": cipher
+            }
+        )
 
     @staticmethod
     def decrypt(cipher):
         pass
 
+### this part does not work .. i could not handle rabbitmq as a message broker
+class MessageBroker:
+    def __init__(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        channel = connection.channel()
+
+    def send_message(self, message):
+        self.channel.basic_publish(
+            exchange='exchange',
+            routing_key='key',
+            body=message
+        )
+        self.connection.close()
+        print("your message has been sent")
+
+    def read_messages(self):
+        pass
+
 
 if __name__ == '__main__':
-    morse = Morse()
+    massage = MessageBroker()
+    massage.send_message('hellow')
+
+    morse = MorseService()
     app.run(host='0.0.0.0', port=1105)
     print(morse.encrypt("Hi Solat"))
+
+
